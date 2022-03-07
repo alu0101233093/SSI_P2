a273a23653bc9c7bbcc62b7d5cc73a5f094492fc
alu0101233093 <alu01012333093@ull.edu.es>
2022-03-07 23:16:42 +0000

    implementado set clave

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,11 @@
-#include "../include/palabra.h"
+#include <bitset>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+const int ASCII_MAX = 90;
+const int ASCII_MIN = 65;
+const int ASCII_RANGE = ASCII_MAX - ASCII_MIN;
 
 std::string clave_random() {
     std::string key;
@@ -9,17 +16,47 @@ std::string clave_random() {
     return key;
 }
 
+std::vector<char> set_clave(std::string pal) {
+    std::vector<char> resultado;
+
+    if(pal.length() > 256){
+        std::cout << "\nLa clave introducida supera los 256 Bytes. Prueba una más corta.\n";
+        return resultado;
+    }
+
+    for(int i = 0, j = 0; i < 256; i++, j++){
+        if(pal[j%pal.length()] != ','){
+            resultado[i] = char(pal[j%pal.length()] - ASCII_MIN);
+        } else { 
+            j++;
+            resultado[i] = char(pal[j%pal.length()] - ASCII_MIN);
+        }
+    }
+    return resultado;
+}
+
+std::vector<int> ksa(std::vector<char> t){
+    std::vector<int> s;
+    for(int i = 0; i < 256; i++){
+        s[i] = i;
+    }
+
+}
+
 int main(){
-    std::string aux;
     int opcion = 0;
     bool clave_asignada = false;
     std::string clave = "";
-    std::palabra pal = "";
+    std::string palabra = "";
+
+    // vector s y t
+    std::vector<int> s(256);
+    std::vector<char> t(256);
 
     do{
         std::cout << "----------------------------------------" << std::endl;
-        std::cout << "Clave: " << clave.get_palabra() << std::endl;
-        std::cout << "Última palabra usada: " << pal.get_palabra() << std::endl;
+        std::cout << "Clave: " << clave << std::endl;
+        std::cout << "Última palabra usada: " << palabra << std::endl;
         std::cout << "----------------------------------------" << std::endl;
         std::cout << "Introduce el número de la opción deseada: \n";
         std::cout << "1. Asignar clave \n";
@@ -32,52 +69,18 @@ int main(){
             case 1:
                     std::cout << "Introduce la nueva clave: ";
                     std::cin >> clave;
+                    t = set_clave(clave);
                     clave_asignada = true;
                     break;
             case 2:
                     clave = clave_random();
+                    t = set_clave(clave);
                     clave_asignada = true;
                     break;
             case 3: if(clave_asignada){
                         std::cout << "Introduce la palabra que deseas codificar: ";
-                        std::cin >> aux;
-                        pal.set_palabra(aux);
-
-                        // Imprimir en decimal
-                        std::cout << "Decimal:       ";
-                        for(int i = 0; i < int(aux.length());i++){
-                            std::cout << pal.get_ascii()[i] << " ";
-                        }
-                        std::cout << std::endl;
-
-                        // Imprimir palabra en binario
-                        std::cout << "Binario:       ";
-                        for(int i = 0; i < int(aux.length());i++){
-                            std::cout << pal.get_binario(i) << " ";
-                        } std::cout << std::endl;
-
-                        // Imprimir clave en binario
-                        std::cout << "Clave binaria: ";
-                        for(int i = 0, j = 0; i < int(aux.length());i++, j++){
-                            if(j >= clave.get_longitud()) j = 0;
-                            std::cout << clave.get_binario(j) << " ";
-                        } std::cout << std::endl;
-
-                        pal = clave.suma(pal);
-                        // Imprimir resultado en binario
-                        std::cout << "Codificación:  ";
-                        for(int i = 0; i < int(aux.length());i++){
-                            std::cout << pal.get_binario(i) << " ";
-                        } std::cout << std::endl;
-
-                        // Imprimir resultado en ascii
-                        std::cout << "Decimal cod.:  ";
-                        for(int i = 0; i < int(aux.length());i++){
-                            std::cout << pal.get_ascii()[i] << " ";
-                        } std::cout << std::endl;
-
-                        // Imprimir palabra
-                        std::cout << "Palabra cod.:  " << pal.get_palabra() << std::endl << std::endl;
+                        std::cin >> palabra;
+                        std::cout << "Palabra cod.:  " << palabra << std::endl << std::endl;
                     }break;
             case 4: break;
             default: std::cout << "Introduce un número del 1 al 4.\n";
